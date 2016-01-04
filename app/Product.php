c342f9916ab5b54916a6d1998a9a17a2b6a17a32
Mauro Lucio <mauro.mls@hotmail.com>
2016-01-04 19:59:38 -0200

    correcoes no fillable do model product e na migration product_tag

--- app/Product.php
+++ app/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['category_id', 'name', 'description', 'price'];
+    protected $fillable = ['category_id', 'name', 'description', 'price','recommend','featured'];
 
     public function category()
     {
